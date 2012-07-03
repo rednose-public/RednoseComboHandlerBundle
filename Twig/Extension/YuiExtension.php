38d5082e3b75142bfe961c162d897cc34c0e30d6
Sven Hagemann <sven@rednose.nl>
2012-07-03 11:20:11 +0200

    Made bundle-side yui loader section aware

--- Twig/Extension/YuiExtension.php
+++ Twig/Extension/YuiExtension.php
@@ -17,7 +17,7 @@ class YuiExtension extends \Twig_Extension
     }
     
     public function getFunctions()
-    {        
+    {
         return array(
             'yui' => new \Twig_Filter_Method($this, 'yuiLoader', array('is_safe' => array('all')))
        );
@@ -27,34 +27,34 @@ class YuiExtension extends \Twig_Extension
     {
         $href = array();
         $buffer = '';
-        
+
         $this->request = $this->container->get('request');
 
         $controller = $this->getRoute();
         $controllerObject = new $controller;
         $controllerAction = $this->getRoute(true);
-        
+
         $controller = substr($controller, strrpos($controller, "\\") + 1);
-        
+
         if ($yuiLoader) {
             $publicPath = $this->request->getBasePath() . '/bundles/' . preg_replace('/bundle$/', '', strtolower($bundle));
             
-            $href[] = $publicPath . '/js/yui.js';            
+            $href[] = $publicPath . '/js/yui.js?section=' . $controllerAction;
 
             $publicPath = $this->request->getBasePath() . '/bundles/' . preg_replace('/bundle$/', '', strtolower('LibbitYuiBundle'));
-            
+
             $href[] = $publicPath . '/yui' . $version . '/yui/yui-min.js';
             $href[] = $publicPath . '/yui-settings.js?locale=' . $this->container->get('session')->getLocale();
         }
-        
+
         $href[] = $this->request->getBaseUrl() . '/javascript/' . $controller . '.js?section=' . $controllerAction;
-        
+
         foreach ($href as $src) {
             $buffer .= '<script src="' . $src . '" /></script>' . "\r\n";
-        }        
-        
+        }
+
         $this->storeVariable($context, $controller, $controllerAction, $bundle);
-        
+
         return $buffer;
     }
 
@@ -65,17 +65,17 @@ class YuiExtension extends \Twig_Extension
     {
         $variable = array();
         $session = $this->container->get('session');
-        
+
         foreach ($context as $key => $value) {
             if (gettype($value) == 'object' && get_class($value) != 'Symfony\Bundle\FrameworkBundle\Templating\GlobalVariables') {
                 $variable[$key] = $value;
             }
-            
+
             if (gettype($value) == 'string' || gettype($value) == 'array') {
                 $variable[$key] = $value;
             }
         }
-        
+
         $session->set('YuiResource-' . $controller . '-' . strtolower($controllerAction), $variable);
         $session->set('YuiResource-BundleName', $bundle);
     }
@@ -85,7 +85,7 @@ class YuiExtension extends \Twig_Extension
         $routeAttributes = $this->request->attributes;
         $routeAttributes = $routeAttributes->all();
         $route = explode("::", $routeAttributes['_controller']);
-        
+
         if (count($route) > 1) {
             if ($action) {
                 $route = str_replace('Action', '', $route[1]);
