0c0ae05d9d86a4f627336067115ec218884ca366
Marc Bontje <marc@rednose.nl>
2012-08-12 16:29:10 +0200

    Cleanup

--- Resources/public/gallery/gallery-libbit-dialog/gallery-libbit-dialog-min.js
+++ Resources/public/gallery/gallery-libbit-dialog/gallery-libbit-dialog-min.js
@@ -1,152 +1,153 @@
-YUI.add('gallery-libbit-dialog', function(Y) {
+YUI.add('gallery-libbit-dialog', function (Y) {
 
     Y.LibbitDialog = {
-        
+
         callback: null,
         callbackCancel: null,
         wHandleCollection: [],
-        
-        Message: function(message, warningMode, callback, headerTitle) {
-            if (typeof warningMode == 'undefined') { warningMode = false; }
-            if (typeof callback == 'undefined') { callback = function() {} }
-            
+
+        Message: function (message, warningMode, callback, headerTitle) {
+            if (typeof warningMode === 'undefined') { warningMode = false; }
+            if (typeof callback === 'undefined') { callback = function () {}; }
+
             this.callback = callback;
-            
-            if (headerTitle == null) {
+
+            if (headerTitle === null) {
                 headerTitle = 'Message';
             }
-            
+
             this.Panel('message', message, headerTitle);
-            
-            if (warningMode == true) {
+
+            if (warningMode === true) {
                 this.Panel.MessageNode
                     .get('parentNode').one('div.dialog_message_icon')
                     .removeClass('dialog_message_icon')
                     .addClass('dialog_warning_icon');
             }
         },
-        
-        Prompt: function(title, fieldName, initialValue, callback, callbackCancel, headerTitle)
-        {
-            if (typeof callback == 'undefined') { callback = function() {} }
-            if (typeof callbackCancel == 'undefined') { callbackCancel = function() { } }
-            
+
+        Prompt: function (title, fieldName, initialValue, callback, callbackCancel, headerTitle) {
+            if (typeof callback === 'undefined') { callback = function () {}; }
+            if (typeof callbackCancel === 'undefined') { callbackCancel = function () {}; }
+
             this.callback = callback;
             this.callbackCancel = callbackCancel;
-            
-            if (headerTitle == null) {
+
+            if (headerTitle === null) {
                 headerTitle = 'Question';
             }
-            
+
             this.Panel('prompt', '...', headerTitle);
-            
-            this.Panel.MessageNode.set(
-                'innerHTML',
+
+            this.Panel.MessageNode.set('innerHTML',
                 '<fieldset>' +
                 ' <legend>' + title +  '</legend>' +
                 ' <label>' + fieldName +  ':</label><br />' +
                 ' <input type="text" value="' + initialValue + '" />' +
-                '</fieldset>'
-            );
+                '</fieldset>');
+
             this.Panel.MessageNode.one('input').focus().select();
-            this.Panel.MessageNode.one('input').on('keyup', function(e) {
-                if (e.button == 13) {
-                        Y.LibbitDialog.Panel.panelObject.hide();
-                        Y.LibbitDialog.callback(
-                            Y.LibbitDialog.Panel.MessageNode.one('input').get('value')
-                        );
-                    }
+            this.Panel.MessageNode.one('input').on('keyup', function (e) {
+                if (e.button === 13) {
+                    Y.LibbitDialog.Panel.panelObject.hide();
+                    Y.LibbitDialog.callback(
+                        Y.LibbitDialog.Panel.MessageNode.one('input').get('value')
+                    );
+                }
             });
         },
-        
-        ErrorMessage: function(errJSON, callback)
-        {
-            var errHTML = '';
-            var err = Y.JSON.parse(errJSON);
 
-            if (typeof callback == 'undefined') { callback = function() {} }
+        ErrorMessage: function (errJSON, callback) {
+            var errHTML = '',
+                err     = Y.JSON.parse(errJSON);
+
+            if (typeof callback === 'undefined') { callback = function () {}; }
 
             this.callback = callback;
-            
+
             errHTML =
                 '<div class="dialog_error">' + err.message + '</div>' +
-                '<div class="dialog_path">Source: ' + err.path + '</div>'
-            
+                '<div class="dialog_path">Source: ' + err.path + '</div>';
+
             this.Panel('error', errHTML, 'Error');
         },
-        
-        Confirm: function(message, callback, callbackCancel, headerTitle)
-        {
-            if (typeof callback == 'undefined') { callback = function() {} }
-            if (typeof callbackCancel == 'undefined') { callbackCancel = function() { } }
-            
+
+        Confirm: function (message, callback, callbackCancel, headerTitle) {
+            if (typeof callback === 'undefined') { callback = function () {}; }
+            if (typeof callbackCancel === 'undefined') { callbackCancel = function () {}; }
+
             this.callback = callback;
             this.callbackCancel = callbackCancel;
-            
-            if (headerTitle == null) {
-                headerTitle = 'Question'
+
+            if (headerTitle === null) {
+                headerTitle = 'Question';
             }
-            
+
             this.Panel('confirm', message, headerTitle);
         },
-        
-        Window: function(windowHandle, title, height, width, content, uri, buttons)
-        {
-            var dialogNode = Y.Node.create('<div id="dialogWindow_' + windowHandle + '" class="dialogWindow" />');
-            
+
+        Window: function (windowHandle, title, height, width, content, uri, buttons) {
+            var dialogNode = Y.Node.create('<div id="dialogWindow_' + windowHandle + '" class="dialogWindow" />'),
+                dialogDispatcher;
+
             Y.one('body').appendChild(dialogNode);
-            
-            if (typeof(content) == 'string') {
+
+            if (typeof (content) === 'string') {
                 dialogNode.set('innerHTML', content);
-            } else if (typeof(content) == 'object' && content !== null) {
+            } else if (typeof (content) === 'object' && content !== null) {
                 dialogNode.append(content);
-            } else if (typeof(uri) == 'string') {
-                var dialogDispatcher = Y.Node.create('<div>Loading...</div>');
-                
+            } else if (typeof (uri) === 'string') {
+                dialogDispatcher = Y.Node.create('<div>Loading...</div>');
+
                 dialogNode.appendChild(dialogDispatcher);
-                
-                new Y.Dispatcher ({
+
+                new Y.Dispatcher({
                     node: dialogDispatcher,
                     ioConfig: {
                         method: 'GET'
                     }
                 }).set('uri',  uri);
             }
-            
+
             this.Window.panelObject = new Y.Panel({
                 srcNode: dialogNode,
                 headerContent: title,
-                width: width, height: height,
-                zIndex: parseInt(Y.all('*').size()),
-                centered: true, modal: true, visible: false, render: true
+                width: width,
+                height: height,
+                zIndex: parseInt(Y.all('*').size(), 10),
+                centered: true,
+                modal: true,
+                visible: false,
+                render: true
             });
             this.Window.panelObject.plug(Y.Plugin.Drag);
-            
+
             this.wHandleCollection[windowHandle] = this.Window.panelObject;
-            
+
             for (i in buttons) {
                 var skipCallback = false;
-                
+
                 this.Window.panelObject.addButton({
                     value     : buttons[i].title,
                     section   : Y.WidgetStdMod.FOOTER,
                     classNames: ['dButton_' + i],
                     action    : function (e) {
                         var currentClass = e.currentTarget._node.getAttribute('class').split(' ');
-                        
+
                         for (x in currentClass) {
                             if (currentClass[x].indexOf('dButton_') > -1) {
-                                if (typeof(buttons[i].callbackClose) === 'boolean') {
+                                if (typeof (buttons[i].callbackClose) === 'boolean') {
                                     if (buttons[i].callbackClose === true) {
                                         Y.LibbitDialog.wHandleCollection[windowHandle].hide();
-                                        
+
                                         skipCallback = true;
                                     }
                                 }
-                                
+
                                 if (skipCallback === false) {
-                                    buttons[parseInt(currentClass[x].replace('dButton_', ''))].callback(
-                                        Y.LibbitDialog.wHandleCollection[windowHandle], dialogNode
+                                    buttons[parseInt(currentClass[x].replace('dButton_', ''), 10)].callback(
+                                        Y.LibbitDialog.wHandleCollection[windowHandle],
+                                        dialogNode
                                     );
                                 }
                             }
@@ -154,7 +155,7 @@ YUI.add('gallery-libbit-dialog', function(Y) {
                     }
                 });
             }
-            
+
             if (dialogNode.all('.yui3-widget-ft').size() > 0) {
                 dialogNode.one('.yui3-widget-ft')
                     .setStyle('position', 'absolute')
@@ -163,47 +164,51 @@ YUI.add('gallery-libbit-dialog', function(Y) {
             }
 
             this.Window.panelObject.show();
-            this.Window.panelObject.on('visibleChange', function() {
+            this.Window.panelObject.on('visibleChange', function () {
                 dialogNode.get('parentNode').remove();
-                
+
                 for (i in buttons) {
-                    if (typeof(buttons[i].callbackClose) === 'boolean') {
+                    if (typeof (buttons[i].callbackClose) === 'boolean') {
                         if (buttons[i].callbackClose === true) {
                             buttons[i].callback(
-                                Y.LibbitDialog.wHandleCollection[windowHandle], dialogNode
+                                Y.LibbitDialog.wHandleCollection[windowHandle],
+                                dialogNode
                             );
                         }
                     }
                 }
             });
         },
-        
-        Panel: function(type, message, headerTitle)
-        {
+
+        Panel: function (type, message, headerTitle) {
             var messageNode = Y.Node.create('<div class="dialogMessage" />');
-            
+
             messageNode.appendChild(Y.Node.create('<div class="dialog_' + type + '_icon" />'));
             messageNode.appendChild(Y.Node.create('<div class="dialog_message">' + message + '</div>'));
-            
+
             Y.one('body').appendChild(messageNode);
 
             this.Panel.panelObject = new Y.Panel({
                 srcNode: messageNode,
                 headerContent: headerTitle,
                 zIndex: Y.all('*').size(),
-                width: 490, centered: true, modal: true, visible: false, render: true
+                width: 490,
+                centered: true,
+                modal: true,
+                visible: false,
+                render: true
             });
             this.Panel.panelObject.plug(Y.Plugin.Drag);
             this.Panel.MessageNode = messageNode.one('div.dialog_message');
-            
-            if (type == 'confirm' || type == 'prompt') {
+
+            if (type === 'confirm' || type === 'prompt') {
                 this.Panel.panelObject.addButton({
                     value  : 'Confirm',
                     section: Y.WidgetStdMod.FOOTER,
                     action : function (e) {
                         Y.LibbitDialog.Panel.panelObject.hide();
-                        
-                        if (type == 'prompt') {
+
+                        if (type === 'prompt') {
                             Y.LibbitDialog.callback(
                                 Y.LibbitDialog.Panel.MessageNode.one('input').get('value')
                             );
@@ -212,7 +217,7 @@ YUI.add('gallery-libbit-dialog', function(Y) {
                         }
                     }
                 });
-                
+
                 this.Panel.panelObject.addButton({
                     value  : 'Cancel',
                     section: Y.WidgetStdMod.FOOTER,
@@ -223,13 +228,14 @@ YUI.add('gallery-libbit-dialog', function(Y) {
                     }
                 });
             }
-            
+
             this.Panel.panelObject.show();
-            this.Panel.panelObject.on('visibleChange', function() {
-                if ((type == 'error' || type == 'message' || type == 'warning') && typeof(this.callback) == 'function') {
+            this.Panel.panelObject.on('visibleChange', function () {
+                if ((type === 'error' || type === 'message' || type === 'warning') && typeof (this.callback) === 'function') {
                     this.callback();
                 }
             });
         }
-    }
+    };
+
 }, '1.0', { requires: ['node', 'dd', 'dd-plugin', 'json-parse', 'panel', 'gallery-dispatcher'] });
