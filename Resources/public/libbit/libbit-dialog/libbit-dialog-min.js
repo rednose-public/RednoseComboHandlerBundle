f779a978cdd9d82b7c11d75b91cec9d99c28da16
Marc Bontje <marc@rednose.nl>
2012-10-31 17:19:11 +0100

    Dialog refactoring, error handling

--- Resources/public/libbit/libbit-dialog/libbit-dialog-min.js
+++ Resources/public/libbit/libbit-dialog/libbit-dialog-min.js
@@ -16,13 +16,68 @@ under the Licence.
 */
 YUI.add('libbit-dialog', function (Y) {
 
-var Dialog;
+/* Dialog class constructor */
+function Dialog(config) {
+    Dialog.superclass.constructor.apply(this, arguments);
+}
 
-Dialog = {
+/*
+ * Required NAME static field, to identify the Widget class and
+ * used as an event prefix, to generate class names etc. (set to the
+ * class name in camel case).
+ */
+Dialog.NAME = "dialog";
 
+/*
+ * The attribute configuration for the Spinner widget. Attributes can be
+ * defined with default values, get/set functions and validator functions
+ * as with any other class extending Base.
+ */
+Dialog.ATTRS = {
+    // An error object
+    error : {
+        value: null
+    }
+};
+
+Y.extend(Dialog, Y.Widget, {
+    // Methods/properties to add to the prototype of the new class
     callback: null,
+
     callbackCancel: null,
+
     wHandleCollection: [],
+
+    initializer: function () {
+        this.after('errorChange', this._setError, this);
+    },
+
+    _setError: function (e) {
+        var error = e.newVal,
+            bb    = this.panel.panelObject.get('boundingBox'),
+            input;
+
+        bb.all('.control-group').each(function (node) {
+            if (node.hasClass('error')) {
+                node.removeClass('error');
+            }
+
+            node.all('.help-block').remove();
+        });
+
+        input = bb.one('[data-path=' + error.path + ']');
+        input.ancestor('.control-group').addClass('error');
+        input.get('parentNode').append('<span class="help-block">' + error.message + '</span>');
+
+        input.focus();
+    },
+
+    /**
+     * Hide the active panel
+     */
+    hide: function() {
+        this.panel.panelObject.destroy();
+    },
 
     message: function (message, warningMode, callback, headerTitle) {
         if (typeof warningMode === 'undefined') { warningMode = false; }
@@ -44,7 +99,9 @@ Dialog = {
         }
     },
 
-    prompt: function (title, fieldName, initialValue, callback, callbackCancel, headerTitle, buttonTitle) {
+    prompt: function (content, fieldName, initialValue, callback, callbackCancel, headerTitle, buttonTitle) {
+        var self = this;
+
         if (typeof callback === 'undefined') { callback = function () {}; }
         if (typeof callbackCancel === 'undefined') { callbackCancel = function () {}; }
 
@@ -59,21 +116,25 @@ Dialog = {
             buttonTitle = 'Confirm';
         }
 
-        this.panel('prompt', '...', headerTitle);
+        this.panel('prompt', '...', headerTitle, buttonTitle);
 
         this.panel.MessageNode.set('innerHTML',
-            '<fieldset>' +
-            ' <legend>' + title +  '</legend>' +
-            ' <label>' + fieldName +  ':</label><br />' +
-            ' <input type="text" value="' + initialValue + '" />' +
-            '</fieldset>');
+            '<div class="form-horizontal">' +
+            '<div class="control-group">' +
+            '<label class="control-label" for="promptInput">' + fieldName + '</label>' +
+            '<div class="controls">' +
+            '<input data-path="' + fieldName.toLowerCase() + '" type="text" id="promptInput" value="' + initialValue + '"/>' +
+            '</div>' +
+            '</div>' +
+            '</div>'
+        );
 
         this.panel.MessageNode.one('input').focus().select();
+
         this.panel.MessageNode.one('input').on('keyup', function (e) {
             if (e.button === 13) {
-                Y.Libbit.Dialog.panel.panelObject.hide();
-                Y.Libbit.Dialog.callback(
-                    Y.Libbit.Dialog.panel.MessageNode.one('input').get('value')
+                self.callback(
+                    self.panel.MessageNode.one('input').get('value')
                 );
             }
         });
@@ -174,7 +235,7 @@ Dialog = {
                         if (currentClass[x].indexOf('dButton_') > -1) {
                             if (typeof (buttons[i].callbackClose) === 'boolean') {
                                 if (buttons[i].callbackClose === true) {
-                                    Y.Libbit.Dialog.wHandleCollection[windowHandle].hide();
+                                    this.wHandleCollection[windowHandle].hide();
 
                                     skipCallback = true;
                                 }
@@ -182,7 +243,7 @@ Dialog = {
 
                             if (skipCallback === false) {
                                 buttons[parseInt(currentClass[x].replace('dButton_', ''), 10)].callback(
-                                    Y.Libbit.Dialog.wHandleCollection[windowHandle],
+                                    this.DialogHandleCollection[windowHandle],
                                     container
                                 );
                             }
@@ -198,7 +259,7 @@ Dialog = {
                 if (typeof (buttons[i].callbackClose) === 'boolean') {
                     if (buttons[i].callbackClose === true) {
                         buttons[i].callback(
-                            Y.Libbit.Dialog.wHandleCollection[windowHandle],
+                            this.wHandleCollection[windowHandle],
                             container
                         );
                     }
@@ -210,17 +271,19 @@ Dialog = {
     },
 
     panel: function (type, message, headerTitle, buttonTitle) {
-        var bodyNode = Y.Node.create('<div/>'),
+        var self = this,
+            bodyNode = Y.Node.create('<div/>'),
             buttons;
 
         if (Y.Lang.isUndefined(buttonTitle)) {
             buttonTitle = 'Confirm';
         }
 
-        bodyNode.appendChild(Y.Node.create('<div class="yui3-widget-bd">' +
-                                           '<div class="icon dialog_' + type + '_icon"></div>' +
-                                           '<div>' + message + '</div>' +
-                                           '</div>'
+        bodyNode.appendChild(Y.Node.create(
+            '<div class="yui3-widget-bd">' +
+            '<div class="icon dialog_' + type + '_icon"></div>' +
+            '<div>' + message + '</div>' +
+            '</div>'
         ));
 
         buttons = [
@@ -228,9 +291,8 @@ Dialog = {
                 value  : 'Cancel',
                 section: Y.WidgetStdMod.FOOTER,
                 action : function (e) {
-                    Y.Libbit.Dialog.panel.panelObject.hide();
-
-                    Y.Libbit.Dialog.callbackCancel();
+                    self.panel.panelObject.hide();
+                    self.callbackCancel();
                 }
             }
         ];
@@ -241,14 +303,12 @@ Dialog = {
                 section: Y.WidgetStdMod.FOOTER,
                 isDefault: true,
                 action : function (e) {
-                    Y.Libbit.Dialog.panel.panelObject.hide();
-
                     if (type === 'prompt') {
-                        Y.Libbit.Dialog.callback(
-                            Y.Libbit.Dialog.panel.MessageNode.one('input').get('value')
+                        self.callback(
+                            self.panel.MessageNode.one('input').get('value')
                         );
                     } else {
-                        Y.Libbit.Dialog.callback();
+                        self.callback();
                     }
                 }
             });
@@ -272,15 +332,16 @@ Dialog = {
         this.panel.panelObject.get('boundingBox').addClass('libbit-dialog');
 
         this.panel.panelObject.show();
+
         /*this.panel.panelObject.on('visibleChange', function () {
             if ((type === 'error' || type === 'message' || type === 'warning') && typeof (this.callback) === 'function') {
                 this.callback();
             }
         });*/
     }
-};
+});
 
 // -- Namespace ----------------------------------------------------------------
 Y.namespace('Libbit').Dialog = Dialog;
 
-}, '1.0', { requires: ['node', 'dd', 'dd-plugin', 'json-parse', 'panel', 'gallery-dispatcher'], skinnable: true });
+}, '1.0', { requires: ['widget', 'node', 'dd', 'dd-plugin', 'json-parse', 'panel', 'gallery-dispatcher'], skinnable: true });
