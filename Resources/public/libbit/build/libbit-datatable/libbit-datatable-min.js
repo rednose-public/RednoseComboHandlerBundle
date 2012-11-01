efbb9713c066083a5a50c37a4f765f2704632241
Marc Bontje <marc@rednose.nl>
2012-11-01 23:11:04 +0100

    libbit-yui3 git submodule

--- Resources/public/libbit/build/libbit-datatable/libbit-datatable-min.js
+++
@@ -1,1 +0,0 @@
-YUI.add("libbit-datatable",function(e,t){var n;n=e.Base.create("dataTable",e.DataTable.Base,[e.DataTable.Scrollable,e.DataTable.Sortable]),e.namespace("Libbit").DataTable=n},"@VERSION@",{requires:["datatable-base","datatable-scroll","datatable-sort"],skinnable:!0});
