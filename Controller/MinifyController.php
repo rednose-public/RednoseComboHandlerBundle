2d9a5657b8c7782828284b06fcec52f204760aeb
Marc Bontje <marc@rednose.nl>
2012-11-17 15:03:57 +0100

    Comboloader poc

--- Controller/MinifyController.php
+++ Controller/MinifyController.php
@@ -44,9 +44,50 @@ class MinifyController extends Controller
             \Minify_Logger::setLogger($errorLogger);
         }
 
-        if ($this->get('request')->get('f') === null) {
-            throw new Exception('No files requested.');
-        }
+        $request = $this->get('request');
+        $files = array_keys($request->query->all());
+
+        // asset('bundles/libbityui')|trim('/')
+        array_walk($files, function (&$file) {
+            $groups = array(
+                'libbit' => array(
+                    'filter' => 'DEBUG',
+                    'pattern' => 'libbit-',
+                    'base' => '/libbityui/',
+                    'root' => 'yui3-libbit/build/'
+                ),
+                'docgenadmin' => array(
+                    'filter' => 'DEBUG',
+                    'pattern' => 'docgenadmin-',
+                    'base' => '/docgenadmin/js/',
+                    'root' => 'docgenadmin/build/'
+                ),
+            );
+
+            $base = 'docgen-standard/web/bundles';
+
+            $file = str_replace(array('_js', '_css'), array('.js', '.css'), $file);
+
+            $group = null;
+
+            foreach ($groups as $g) {
+                $pattern = $g['pattern'];
+                $root = $g['root'];
+
+                if (substr($file, strlen($root), strlen($pattern)) === $pattern) {
+                    $group = $g;
+                }
+            }
+
+            if ($group !== null) {
+                $file = $base.$group['base'].$file;
+            } else {
+                $file = $base.'/libbityui/'.$file;
+            }
+        });
+
+        $_GET = array();
+        $_GET['f'] = implode(',', $files);
 
         ini_set('zlib.output_compression', '0');
 
@@ -59,10 +100,22 @@ class MinifyController extends Controller
 
     protected function getOptions()
     {
-        return array(
+        $env = $this->get('kernel')->getEnvironment();
+
+        $options = array(
+            // Output an array of data instead returning headers/content
             'quiet' => true,
-            // TODO: Don't minify when in prod, just combine.
-            'debug' => true,
+            'debug' => $env === 'dev',
         );
+
+        if ($env === 'prod') {
+            // Don't minify in prod, just combine. 'minifiers' => array(
+            $options['minifiers'] = array(
+                \Minify::TYPE_CSS => '',
+                \Minify::TYPE_JS  => '',
+            );
+        }
+
+        return $options;
     }
 }
